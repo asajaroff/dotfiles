49f69691cfbff30c84bf1dabcc3614a7ce7dd53c
Alejandro Sajaroff <asajaroff@gmail.com>
2019-03-17 13:52:07 -0300

    Added zshrc symlink creation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# ZSH setup
+echo "Installing ZSH"
+# CURL
+echo "Generating symlinks for ~/.zshrc" 
+ln -sf ~/.dotfiles/zshrc ~/.zshrc
+
 # vim setup
 echo "Creating necesary directories for vim's swapfiles, backupfiles and undodir"
 mkdir -p $HOME/.vim/swapfiles $HOME/.vim/swapfiles $HOME/.vim/backupfiles $HOME/.vim/undodir
