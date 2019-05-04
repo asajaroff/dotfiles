5e5a22cec8cd7088892691f98bef11e794b55a54
Alejandro Sajaroff <asajaroff@gmail.com>
2019-05-03 22:51:45 -0300

    switched to a generic OS install

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-# Curl
-echo "Installing curl"
-pacman -Syu curl
+# zsh theming
+echo "General configurations"
+mkdir -p ~/Workspace/Logs ~/Workspace/minikube ~/Workspace/Scripts ~/Workspace/Temp ~/Workspace/Repos
 
-# ZSH setup
-echo "Installing ZSH"
-# CURL
+# echo "Installing ZSH"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo "Generating symlinks for ~/.zshrc" 
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
+echo "Installing Bullet train theme"
+wget http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme
+mv bullet-train.zsh-theme $ZSH_CUSTOM/themes
 
 # vim setup
 echo "Creating necesary directories for vim's swapfiles, backupfiles and undodir"
@@ -15,56 +17,37 @@ mkdir -p $HOME/.vim/swapfiles $HOME/.vim/swapfiles $HOME/.vim/backupfiles $HOME/
 echo "Generating symlink to vimrc"
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 
-# Install all needed software
-# sudo pacman -Syu npm jre
+# Install SRE Tools
 
-# Install SRE software
-# aws cli
 echo "Installing aws cli for Linux"
 curl -O https://bootstrap.pypa.io/get-pip.py && python3 get-pip.py --user && pip install awscli --upgrade --user
-
-# aws-iam-authenticator
-echo "Installing aws-iam-authenticator"
+echo "aws-iam-authenticator"
 curl https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/linux/amd64/aws-iam-authenticator -o aws-iam-authenticator
 mv aws-iam-authenticator $HOME/.local/bin/aws-iam-authenticator
 chmod +x $HOME/.local/bin/aws-iam-authenticator
-
-# kubectl
-echo "Installing kubectl"
-sudo apt-get update && sudo apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
-
+echo "kubectl"
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x kubectl
 # kubens & kubectx
-echo "Installing kubens and kubectx"
+echo "kubens and kubectx"
 curl https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx -o kubectx
 curl https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens -o kubens
 chmod +x kube* 
 mv kube* $HOME/.local/bin/
-
-# Terraform 
-echo "Installing Terraform binary"
+echo "Terraform"
 curl https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip -o terraform.zip
 unzip terraform.zip
 chmod +x terraform
 mv terraform $HOME/.local/bin/
-
-# sops
-echo "Installing mozilla sops"
+echo "Mozilla SOPS"
 wget https://github.com/mozilla/sops/releases/download/3.2.0/sops-3.2.0.linux
 chmod +x sops-3.2.0.linux
 mv sops $HOME/.local/bin/sops
-
-# stern
-echo "Installing stern"
+echo "stern"
 wget https://github.com/wercker/stern/releases/download/1.10.0/stern_linux_amd64
 chmod +x stern_linux_amd64
 mv stern_linux_amd64 $HOME/.local/bin/stern
-# Pendientes
-
-# Helm
+echo "helm"
 wget https://storage.googleapis.com/kubernetes-helm/helm-v2.9.1-linux-amd64.tar.gz
 tar -xvf helm-v2.9.1-linux-amd64.tar.gz
 chmod +x ./linux-amd64/helm 
