13fe789b3164291963ade83c030988c177f64d4c
Alejandro Sajaroff <asajaroff@gmail.com>
2020-04-01 08:48:05 -0300

    Initial commit

--- functions/kubernetes.sh
+++ functions/kubernetes.sh
@@ -7,3 +7,8 @@ function kube-get-env {
   aws eks update-kubeconfig --name $EKS_NAME --kubeconfig $HOME/.kube/$KUBECONFIG_FILE
   export KUBECONFIG=$HOME/.kube/$KUBECONFIG_FILE
 }
+
+# kubectl get pods -o json | jq -r '.items | sort_by(.spec.nodeName)[] | [.spec.nodeName,.metadata.name] '
+
+# Filter by node
+# kubectl get pod -o json | jq -r '.items[] | select(.spec.nodeName==ip-172-30-63-169.ec2.internal)' 
