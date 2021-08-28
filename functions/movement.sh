c2162400805f7dee4f966b9936e0c243d1692508
asajaroff <asajaroff@users.noreply.github.com>
2021-08-27 23:35:41 -0300

    Removed buggy function

--- functions/movement.sh
+++ functions/movement.sh
@@ -1,25 +1,10 @@
-func repos () {
-	if [ -z "$1" ]; then
-		if  [ "${PWD}" = "${REPOS}" ]; then
-			echo "Already at repos folder"
-		else
-			cd $REPOS
-			ls -1
-		fi
-	else
-		DIR=${1}
-		cd "${REPOS}/${DIR}"
-		ls -1
-	fi
-}
-
 func tmpfile() {
   FILENAME=`date "+%Y-%m-%d_%H:%M:%S"`
   if [ -z "$1" ]; then
     touch ~/Workspace/tmp/$1.md
-    code ~/Workspace/tmp/$1.md
+    nvim ~/Workspace/tmp/$1.md
   else
     touch ~/Workspace/tmp/${FILENAME}.md
-    vim ${HOME}/Workspace/tmp/${FILENAME}.md
+    nvim ${HOME}/Workspace/tmp/${FILENAME}.md
   fi
 }
