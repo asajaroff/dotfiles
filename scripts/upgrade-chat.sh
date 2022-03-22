787e20247bf1e88e8000293f422c2c5388add81c
Alejandro Sajaroff <asajaroff@users.noreply.github.com>
2022-03-22 13:25:04 -0300

    Initial commit for file

---
+++ scripts/upgrade-chat.sh
@@ -0,0 +1,11 @@
+cat << EOF > /usr/share/applications/teamwork-chat.desktop
+[Desktop Entry]
+Name=Teamwork Chat
+GenericName=Chat client
+Comment=Chat client
+Exec=/opt/TeamworkChat/TeamworkChat-1.20.2.AppImage --no-sandbox
+Icon=/opt/TeamworkChat/icon.png
+Type=Application
+Terminal=false
+EOF
+
